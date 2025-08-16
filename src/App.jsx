68bc2ec31fede7f590c22fe018f5cd61e4a267f1
Nazi <banonazisharifi@gmail.com>
2025-08-16 10:58:49 +0430

    React-router-dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route index element=<Home/> />
+        <Route path='/about' element=<About/> />
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
