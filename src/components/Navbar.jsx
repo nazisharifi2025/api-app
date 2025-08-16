f0b60a6c5a1d6edca2e30ce656e42ef6cc3b5b3a
Nazi <banonazisharifi@gmail.com>
2025-08-16 22:42:24 +0430

    React-router-dom added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 export default function Navbar(){
     return(
         <div className="h-10 w-full shadow-sm flex justify-between items-center">
-
+            <nav>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Serves</li>
+                <li>Contact</li>
+            </nav>
         </div>
     )
 }
